417e1304e6fd7c95a6973dc210e6b3025775c995
Vishnu Sundaresan <68603272+juliussneezer04@users.noreply.github.com>
2023-07-05 19:50:06 -0400

    advanced cases

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from "react";
+import renderer from "react-test-renderer";
 
-import App from '../App';
+import App from "../App";
+import { divideNumbers } from "../lib/utils";
 
-describe('<App />', () => {
-  it('has 1 child', () => {
+describe("<App />", () => {
+  it("has 5 child", () => {
     const tree = renderer.create(<App />).toJSON();
-    console.log("CHILDREN", tree.children);
-    expect(tree.children.length).toBe(1);
+    expect(tree.children.length).toBe(5);
+  });
+});
+
+
+describe("Division Function", () => {
+  // NOTE: Use %d for numbers, %s for strings, %j for JSON
+  it("can divide non-zero numbers", () => {
+    const num = 10;
+    const denom = 2;
+    const expected = 5;
+    const res = divideNumbers(num, denom);
+    expect(res).toBe(expected);
+  });
+
+  
+  it("throws an error when dividing by zero", () => {
+    expect(() => divideNumbers(1, 0)).toThrow();
   });
 });
